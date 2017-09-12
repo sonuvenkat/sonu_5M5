044530f6a7d04f7578bf8f0490631308014bcc9b
sonuvenkat <narasimha7796@gmail.com>
2017-09-12 12:27:26 +0530

    erro

--- Main.java
+++ Main.java
@@ -2,8 +2,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
- StudentGroup sg;
-sg.getStudents();
+ 
 		//You may test that your code works find here
 		//Please check that your code works and has no 
 		//compilation problems before to submit
